d2419b2bfc6626596104a99454aff33d85b8c37b
jhgonzalezuruguay <102374190+jhgonzalezuruguay@users.noreply.github.com>
2025-05-10 10:20:56 -0300

    Update app.py
    Signed-off-by: jhgonzalezuruguay <102374190+jhgonzalezuruguay@users.noreply.github.com>

--- app.py
+++ app.py
@@ -8,13 +8,14 @@ import os
 #import streamlit as st
 
 # Retrieve the token from Streamlit secrets
-github_token = st.secrets["GITHUB_TOKEN"]
+#github_token = st.secrets["GITHUB_TOKEN"]
 
 # Construct the repository URL
-repo_url = f"https://{github_token}@github.com/jhgonzalezuruguay/SALUD_MENTAL_2.git"
+#repo_url = f"https://{github_token}@github.com/jhgonzalezuruguay/SALUD_MENTAL_2.git"
 
 # Use the URL securely in your application
-print(f"Repository URL: {repo_url[:50]}... (truncated for security)")
+#print(f"Repository URL: {repo_url[:50]}... (truncated for security)")
+
 # Inicializar archivo CSV si no existe
 def inicializar_csv():
     if not os.path.exists(CSV_FILE):
